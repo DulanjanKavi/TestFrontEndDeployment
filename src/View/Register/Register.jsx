e2cf82571db39833404e3edf77b20bca0c2d8b3d
Dulanjan Kavindu <DulanjanKavindu.dev@outlook.com>
2025-06-07 11:39:21 +0530

    change register page

--- src/View/Register/Register.jsx
+++ src/View/Register/Register.jsx
@@ -4,6 +4,7 @@ const Register = () => {
     return (
         <div className='bg-green-200 text-black'>
             Register page<br />next line
+            yes i deployee it
         </div>
     )
 }
